c4451a071f30b4529c2ec182b7fcfbe0d325858d
Mitja Horvat <pinkfluid@gmail.com>
2011-07-19 01:37:57 +0200

    Change some debug messages.

--- src/console.c
+++ src/console.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 
 #include "console.h"
 #include "txtbuf.h"
@@ -31,11 +32,20 @@ void con_dump(void)
 {
     int ii;
 
-    printf("=========================================\n");
+    size_t slen = 0;
+
+    printf("===== [ CONSOLE TXTBUF: %d %d, total %d ] =============\n",
+           con_tb.tb_head,
+           con_tb.tb_tail,
+           con_tb.tb_tail - con_tb.tb_head);
+
     for (ii = 0; tb_strget(&con_tb, ii, con_str, sizeof(con_str)); ii++)
     {
         fputs(con_str, stdout);
+        slen += strlen(con_str);
     }
+
+    printf("===== [ CONSOLE TOTAL: Strlen=%d, numlines=%d ] =======\n", slen, ii);
 
     fflush(stdout);
 }
