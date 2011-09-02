30ed924aaddce09e6346b1b34e201cc4e8c0d602
Mitja Horvat <mitja.horvat@aviatnet.com>
2011-09-02 11:37:02 +0200

    Minor fixes to the help text.

--- src/help.c
+++ src/help.c
@@ -21,17 +21,17 @@ const char *help_chatlog_warning =
 "\n"
 "The chatlog feature is required for this application to work properly,\n"
 "but it is currently disabled.\n"
+"\n"
 "This application can enable the chatlog feature for you, but keep in\n"
-"mind that  using it was never officially aproved by NCSoft.\n"
+"mind that using it was never officially aproved by NCSoft.\n"
 "\n"
-"Said that, you most probably WONT get banned, but please be aware\n"
-"that you are walking on the gray area until there is an official answer\n"
-"by NCSoft.\n"
+"Most likely WILL NOT get banned, but please be aware that you are \n"
+"walking on the gray area until there is an official answer by NCSoft.\n"
 "\n"
-"This application is no more illegal than any DPS meter is, since the\n"
-"principles behind are the same.\n"
-"\n"
-"!!!!! WARNING!!!!!! !!!!! WARNING!!!!!! !!!!! WARNING!!!!!! \n";
+"The legality status of this application falls in the same category\n"
+"as the DPS meters, since the principles behind are the same (chatlog\n"
+"parsing). The difference is only in that I was kind enough to warn you :)\n"
+"\n";
 
 const char *help_chatlog_enabled =
 "The chatlog feature was enabled in AION.\n"
