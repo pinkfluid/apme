fdd3a1ed4e9ecb438ac4649534ed56330d624380
Mitja Horvat <mitja.horvat@aviatnet.com>
2011-09-02 21:45:50 +0200

    Add version file.

---
+++ src/version.h
@@ -0,0 +1,17 @@
+#ifndef VERSION_H_INCLUDED
+#define VERSION_H_INCLUDED
+
+#define APME_VERSION_MAJOR      0
+#define APME_VERSION_MINOR      2
+#define APME_VERSION_REVISION   0
+#define APME_VERSION_NAME       "Nanos"
+
+#define __APM_STR(x)            #x
+#define APM_STR(x)              __APM_STR(x)
+
+#define APME_VERSION_STRING  APM_STR(APME_VERSION_MAJOR) "." \
+                             APM_STR(APME_VERSION_MINOR) "." \
+                             APM_STR(APME_VERSION_REVISION) " "\
+                             "(" APME_VERSION_NAME ")"
+
+#endif /* VERSION_H_INCLUDED */
